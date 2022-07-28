cdc59781820e868fc375c9991447426d9827a645
PEARLNIE <46552356+PEARLNIE@users.noreply.github.com>
2022-07-28 22:35:57 +0800

    fix execute_container.R

--- R/execute_container.R
+++ R/execute_container.R
@@ -3,7 +3,9 @@
 #' @importFrom babelwhale list_docker_images pull_container
 #' @importFrom tidyr unite
 #' @importFrom dplyr pull
-
+#'
+#' @export
+#'
 execute_container <- function(ref_data, verbose) {
 
   # Process file path-----------------------------------------------------------
